dc446e07a4fb3f3f1c45d3bb33ae9653adfff67e
Not-Nova-Shane <ezpz12453@gmail.com>
2025-07-06 19:20:08 -0400

    Add files via upload

---
+++ LetheMain.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BattleUI.Dialog;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using TheFunGang.patches;
+
+namespace TheFunGang;
+
+[BepInPlugin("shane.TheFunGang", "The Fun Gang Support Passives", "1.0.0")]
+public class Main : BasePlugin
+{
+    public override void Load()
+    {
+        Harmony harmony = new Harmony("The Fun Gang Support Passives");
+        Passive_FunGang.Setup(harmony);
+    }
+
+
+}
