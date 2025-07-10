4d70b7aa6a5f6221e0e49470c842e86698d3dc33
Not-Nova-Shane <ezpz12453@gmail.com>
2025-07-10 19:41:29 -0400

    Add files via upload

---
+++ Lethe/LetheMain.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BattleUI.Dialog;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using TheFunGang.patches;
+
+namespace TheFunGang;
+
+[BepInPlugin("shane.TheFunGang", "TFG Kris and Susie", "1.0.1")]
+public class Main : BasePlugin
+{
+    public override void Load()
+    {
+        Harmony harmony = new Harmony("Kris Support Passive");
+        Passive_Kris.Setup(harmony);
+        Harmony SusieHarmony = new Harmony("Susie Support Passive");
+        Passive_Susie.Setup(SusieHarmony);
+    }
+
+
+}
